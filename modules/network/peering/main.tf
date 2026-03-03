e33dfe5da26285f9d155b23fa20484e434ce5fa0
Filip Dabrowski <filip.a.dabrowski@gmail.com>
2026-03-03 14:37:31 +0100

    update(network): started preparing VPN Gatway
    - change variables names to more descriptive
    - allow gateway traffic for hub
    - use remote gateway for spoke

--- modules/network/peering/main.tf
+++ modules/network/peering/main.tf
@@ -1,15 +1,17 @@
 resource "azurerm_virtual_network_peering" "vnet1-to-vnet2" {
-  name                      = "peer-${var.vnet1_name}-to-${var.vnet2_name}"
-  resource_group_name       = var.vnet1_rg_name
-  virtual_network_name      = var.vnet1_name
-  remote_virtual_network_id = var.vnet2_id
+  name                      = "peer-${var.vnet1_name_hub}-to-${var.vnet2_name_spoke}"
+  resource_group_name       = var.vnet1_rg_name_hub
+  virtual_network_name      = var.vnet1_name_hub
+  remote_virtual_network_id = var.vnet2_id_spoke
   allow_forwarded_traffic   = true
+  allow_gateway_transit     = true
 }
 
 resource "azurerm_virtual_network_peering" "vnet2-to-vnet1" {
-  name                      = "peer-${var.vnet2_name}-to-${var.vnet1_name}"
-  resource_group_name       = var.vnet2_rg_name
-  virtual_network_name      = var.vnet2_name
-  remote_virtual_network_id = var.vnet1_id
+  name                      = "peer-${var.vnet2_name_spoke}-to-${var.vnet1_name_hub}"
+  resource_group_name       = var.vnet2_rg_name_spoke
+  virtual_network_name      = var.vnet2_name_spoke
+  remote_virtual_network_id = var.vnet1_id_hub
   allow_forwarded_traffic   = true
+  use_remote_gateways       = true
 }
--- modules/network/peering/varibales.tf
+++ modules/network/peering/varibales.tf
@@ -1,25 +1,25 @@
 # variables of the 1st vnet
-variable "vnet1_name" {
+variable "vnet1_name_hub" {
   type = string
 }
 
-variable "vnet1_id" {
+variable "vnet1_id_hub" {
   type = string
 }
 
-variable "vnet1_rg_name" {
+variable "vnet1_rg_name_hub" {
   type = string
 }
 
 # variables of the 2nd vnet
-variable "vnet2_name" {
+variable "vnet2_name_spoke" {
   type = string
 }
 
-variable "vnet2_id" {
+variable "vnet2_id_spoke" {
   type = string
 }
 
-variable "vnet2_rg_name" {
+variable "vnet2_rg_name_spoke" {
   type = string
 }
