cb10554fd5dff00473c749a97e9ea1e03f83df48
Filip Dabrowski <filip.a.dabrowski@gmail.com>
2026-03-05 08:35:04 +0100

    add private IPs of web servers as an output

--- modules/compute/vmss/output.tf
+++ modules/compute/vmss/output.tf
@@ -1,3 +1,8 @@
 output "web_servers_id" {
   value = azurerm_linux_virtual_machine_scale_set.web_servers.id
 }
+
+output "web_servers_ips" {
+  value = azurerm_linux_virtual_machine_scale_set.web_servers.network_interface[*].ip_configuration[0].private_ip_address
+  description = "list of pivate IPs of machines in VMSS"
+}
